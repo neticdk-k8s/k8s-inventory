82faae236bd53861cebdfe7b1244de8a7e137730
Kim Nørgaard <kn@netic.dk>
2023-05-10 18:50:05 +0200

    fix up more structs

--- calico.go
+++ calico.go
@@ -4,11 +4,16 @@ type Calico struct {
 	TypeMeta
 	ObjectMeta
 
+	Spec CalicoSpec `json:"spec" db:"spec"`
+}
+
+type CalicoSpec struct {
 	Version string `json:"version"`
 }
 
 func NewCalico() *Calico {
 	return &Calico{
 		ObjectMeta: NewObjectMeta(),
+		Spec:       CalicoSpec{},
 	}
 }
--- velero.go
+++ velero.go
@@ -20,30 +20,46 @@ type VeleroSchedule struct {
 	TypeMeta
 	ObjectMeta
 
-	Schedule           string          `json:"schedule" db:"schedule"`
+	Spec   VeleroScheduleSpec   `json:"spec" db:"spec"`
+	Status VeleroScheduleStatus `json:"status" db:"status"`
+}
+
+type VeleroScheduleSpec struct {
+	Schedule           string          `json:"schedule"`
 	ExcludedNamespaces []string        `json:"excluded_namespaces"`
-	SnapshotVolumes    *bool           `json:"snapshot_volumes" db:"snapshot_volumes"`
-	TTL                metav1.Duration `json:"ttl" db:"ttl"`
-	LastBackup         *metav1.Time    `json:"last_backup" db:"last_backup"`
-	Phase              string          `json:"phase" db:"phase"`
+	SnapshotVolumes    *bool           `json:"snapshot_volumes"`
+	TTL                metav1.Duration `json:"ttl"`
+	LastBackup         *metav1.Time    `json:"last_backup"`
+}
+
+type VeleroScheduleStatus struct {
+	Phase string `json:"phase" db:"phase"`
 }
 
 type VeleroBackup struct {
 	TypeMeta
 	ObjectMeta
 
-	ScheduleName        string          `json:"schedule_name" db:"schedule_name"`
-	ExcludedNamespaces  []string        `json:"excluded_namespaces"`
-	StorageLocation     string          `json:"storage_location" db:"storage_location"`
-	SnapshotVolumes     *bool           `json:"snapshot_volumes" db:"snapshot_volumes"`
-	TTL                 metav1.Duration `json:"ttl" db:"ttl"`
-	StartTimestamp      *metav1.Time    `json:"start_timestamp" db:"start_timestamp"`
-	CompletionTimestamp *metav1.Time    `json:"completion_timestamp" db:"completion_timestamp"`
-	Expiration          *metav1.Time    `json:"expiration" db:"expiration"`
-	Phase               string          `json:"phase" db:"phase"`
-	ItemsBackedUp       int             `json:"items_backed_up" db:"items_backed_up"`
-	TotalItems          int             `json:"total_items" db:"total_items"`
-	Warnings            int             `json:"warnings" db:"warnings"`
-	Errors              int             `json:"errors" db:"errors"`
-	Version             int             `json:"version" db:"version"`
+	Spec   VeleroBackupSpec   `json:"spec" db:"spec"`
+	Status VeleroBackupStatus `json:"status" db:"status"`
+}
+
+type VeleroBackupSpec struct {
+	ScheduleName       string          `json:"schedule_name" db:"schedule_name"`
+	ExcludedNamespaces []string        `json:"excluded_namespaces"`
+	StorageLocation    string          `json:"storage_location" db:"storage_location"`
+	SnapshotVolumes    *bool           `json:"snapshot_volumes" db:"snapshot_volumes"`
+	TTL                metav1.Duration `json:"ttl" db:"ttl"`
+}
+
+type VeleroBackupStatus struct {
+	StartTimestamp      *metav1.Time `json:"start_timestamp" db:"start_timestamp"`
+	CompletionTimestamp *metav1.Time `json:"completion_timestamp" db:"completion_timestamp"`
+	Expiration          *metav1.Time `json:"expiration" db:"expiration"`
+	Phase               string       `json:"phase" db:"phase"`
+	ItemsBackedUp       int          `json:"items_backed_up" db:"items_backed_up"`
+	TotalItems          int          `json:"total_items" db:"total_items"`
+	Warnings            int          `json:"warnings" db:"warnings"`
+	Errors              int          `json:"errors" db:"errors"`
+	Version             int          `json:"version" db:"version"`
 }
