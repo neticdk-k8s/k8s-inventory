2b3314a8828bf12db9c2578a386bd8de507b662d
Kim Nørgaard <kn@netic.dk>
2023-05-24 20:23:50 +0200

    Implement Valuer and Scanner interfaces to allow (de)serialization with db

--- cron_job.go
+++ cron_job.go
@@ -1,6 +1,12 @@
 package inventory
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 type CronJobSpec struct {
 	Schedule          string       `json:"schedule"`
@@ -8,9 +14,37 @@ type CronJobSpec struct {
 	JobTemplate       *PodTemplate `json:"job_template"`
 }
 
+func (cs *CronJobSpec) Value() (driver.Value, error) {
+	bytes, err := json.Marshal(cs)
+	return bytes, err
+}
+
+func (cs *CronJobSpec) Scan(val interface{}) error {
+	b, ok := val.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &cs)
+}
+
 type CronJobStatus struct {
 	LastScheduleTime   *metav1.Time `json:"last_schedule_time,omitempty"`
 	LastSuccessfulTime *metav1.Time `json:"last_successful_time,omitempty"`
+}
+
+func (cs *CronJobStatus) Value() (driver.Value, error) {
+	bytes, err := json.Marshal(cs)
+	return bytes, err
+}
+
+func (cs *CronJobStatus) Scan(val interface{}) error {
+	b, ok := val.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &cs)
 }
 
 func NewCronJob() *Workload {
--- deployment.go
+++ deployment.go
@@ -1,11 +1,31 @@
 package inventory
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 type DeploymentSpec struct {
 	Replicas *int32       `json:"replicas"`
 	Strategy string       `json:"strategy"`
 	Template *PodTemplate `json:"template"`
+}
+
+func (ds *DeploymentSpec) Value() (driver.Value, error) {
+	bytes, err := json.Marshal(ds)
+	return bytes, err
+}
+
+func (ds *DeploymentSpec) Scan(val interface{}) error {
+	b, ok := val.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &ds)
 }
 
 type DeploymentStatus struct {
@@ -14,6 +34,20 @@ type DeploymentStatus struct {
 	UpdatedReplicas     int32 `json:"updated_replicas"`
 	AvailableReplicas   int32 `json:"available_replicas"`
 	UnavailableReplicas int32 `json:"unavailable_replicas"`
+}
+
+func (ds *DeploymentStatus) Value() (driver.Value, error) {
+	bytes, err := json.Marshal(ds)
+	return bytes, err
+}
+
+func (ds *DeploymentStatus) Scan(val interface{}) error {
+	b, ok := val.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(b, &ds)
 }
 
 func NewDeployment() *Workload {
