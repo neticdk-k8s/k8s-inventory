668f29de01798df34df13eae9a6ee1cb1f595b9b
Kim Nørgaard <kn@netic.dk>
2024-01-04 12:09:51 +0100

    All subscription IDs are strings

--- tenant.go
+++ tenant.go
@@ -4,7 +4,7 @@ type Tenants []*Tenant
 
 type Tenant struct {
 	Name                     string `json:"name" validate:"required" db:"name"`
-	SubscriptionID           int    `json:"subscription_id" validate:"required" db:"subscription_id"`
+	SubscriptionID           string `json:"subscription_id" validate:"required" db:"subscription_id"`
 	Namespace                string `json:"namespace" validate:"required" db:"namespace"`
 	BusinessUnitID           string `json:"business_unit_id" db:"business_unit_id"`
 	HasApplicationManagement bool   `json:"has_application_management" db:"has_application_management"`
