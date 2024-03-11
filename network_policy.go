ab4eee4605fad63303cdafb3d1296133e46b0a98
Kim Nørgaard <kn@netic.dk>
2024-03-11 09:34:19 +0100

    Add (un)marshal for json serialization

--- network_policy.go
+++ network_policy.go
@@ -23,6 +23,81 @@ type NetworkPolicyPort struct {
 	EndPort  *int32       `json:"endPort,omitempty"`
 }
 
+func (npp NetworkPolicyPort) MarshalJSON() ([]byte, error) {
+	if npp.Port == nil {
+		return json.Marshal(struct {
+			Protocol *string      `json:"protocol,omitempty"`
+			Port     *IntOrString `json:"port,omitempty"`
+			EndPort  *int32       `json:"endPort,omitempty"`
+		}{
+			Protocol: npp.Protocol,
+			Port:     nil,
+			EndPort:  npp.EndPort,
+		})
+	}
+	if npp.Port.Type == 0 { // int
+		return json.Marshal(struct {
+			Protocol *string `json:"protocol,omitempty"`
+			Port     *int32  `json:"port,omitempty"`
+			EndPort  *int32  `json:"endPort,omitempty"`
+		}{
+			Protocol: npp.Protocol,
+			Port:     &npp.Port.IntVal,
+			EndPort:  npp.EndPort,
+		})
+	}
+	return json.Marshal(struct {
+		Protocol *string `json:"protocol,omitempty"`
+		Port     *string `json:"port,omitempty"`
+		EndPort  *int32  `json:"endPort,omitempty"`
+	}{
+		Protocol: npp.Protocol,
+		Port:     &npp.Port.StrVal,
+		EndPort:  npp.EndPort,
+	})
+}
+
+func (npp *NetworkPolicyPort) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" || string(data) == `""` {
+		return nil
+	}
+
+	var jsonNPP struct {
+		Protocol *string      `json:"protocol,omitempty"`
+		Port     *interface{} `json:"port,omitempty"`
+		EndPort  *int32       `json:"endPort,omitempty"`
+	}
+
+	if err := json.Unmarshal(data, &jsonNPP); err != nil {
+		return err
+	}
+
+	var port *IntOrString
+
+	portVal := *jsonNPP.Port
+
+	switch v := portVal.(type) {
+	case *int32:
+		port = &IntOrString{
+			Type:   0,
+			IntVal: *v,
+		}
+	case *string:
+		port = &IntOrString{
+			Type:   1,
+			StrVal: *v,
+		}
+	}
+
+	*npp = NetworkPolicyPort{
+		Protocol: jsonNPP.Protocol,
+		Port:     port,
+		EndPort:  jsonNPP.EndPort,
+	}
+
+	return nil
+}
+
 type IPBlock struct {
 	CIDR   string   `json:"cidr,omitempty"`
 	Except []string `json:"except,omitempty"`
